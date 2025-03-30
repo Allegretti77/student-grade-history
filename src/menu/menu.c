65034ea0ba8d15f647afc070219566e70ba0a79b
wvxbs <gabriel.ferreira7854@gmail.com>
2025-03-30 17:59:39 -0300

    user input validation added to the menu

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "menu.h"
 #include "sorter.h"
 #include "csv_handler.h"
 
 void exibirMenu(Aluno *alunos, int quantidade) {
     int opcao;
+    char entrada[100];
+
     do {
         printf("\nMenu de Ordenação\n");
         printf("1 - Ordenar por Nome\n");
@@ -13,8 +16,16 @@ void exibirMenu(Aluno *alunos, int quantidade) {
         printf("4 - Ordenar por Disciplina e Média Final (decrescente)\n");
         printf("5 - Ordenar por Período, Semestre, Turma, Disciplina e Nome\n");
         printf("6 - Sair\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &opcao);
+
+        do {
+            printf("Escolha uma opção: ");
+            fgets(entrada, sizeof(entrada), stdin);
+
+            if (sscanf(entrada, "%d", &opcao) != 1) {
+                printf("Entrada inválida. Digite um número entre 1 e 6.\n");
+                opcao = 0;
+            }
+        } while (opcao < 1 || opcao > 6);
 
         switch (opcao) {
             case 1:
@@ -41,8 +52,6 @@ void exibirMenu(Aluno *alunos, int quantidade) {
                 salvarCSV("saida.csv", alunos, quantidade);
                 printf("Saindo...\n");
                 break;
-            default:
-                printf("Opção inválida. Tente novamente.\n");
         }
     } while (opcao != 6);
 }
